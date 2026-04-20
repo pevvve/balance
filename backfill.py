5d1b7a3609d9184bde0711ca65cacc1dbec62b83
pevvve <pevvve@protonmail.com>
2026-04-20 16:32:01 -0600

    Update backfill.py

--- backfill.py
+++ backfill.py
@@ -140,8 +140,8 @@ def main():
         return
 
     # FULL DATE RANGE
-    start_date = datetime.date(2026, 3, 10)
-    end_date = datetime.date(2026, 3, 30)
+    start_date = datetime.date(2026, 4, 1)
+    end_date = datetime.date(2026, 4, 20)
     
     current_date = start_date
     while current_date <= end_date:
